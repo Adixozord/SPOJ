44fef2978a552796351f248095dc8b3076abdf31
Adixozord <falenczyk.adrian@gmail.com>
2019-02-03 10:53:49 +0000

    Update Sumator.java
    updated the code to pass the output test

--- Sumator.java
+++ Sumator.java
@@ -4,25 +4,42 @@ import java.util.stream.IntStream;
 public class Sumator {
 
 	public static void main(String[] args) {
-		
+
 		Scanner scan = new Scanner(System.in);
-		System.out.println("How many cases am i dealing with?");
-		int cases = scan.nextInt();
+		// System.out.println("How many cases am i dealing with?");
+		int cases = 0;
 		
-		for(int i = 0; i < cases; i++) {
-			
-			System.out.println("How many nums are we adding today?");
-			int numOfNums = scan.nextInt();
-
-			int[] allNums = new int[numOfNums];
-
-			for(int j = 0; j < numOfNums; j++) {
-				int num = scan.nextInt();
-				allNums[j] = num;
+		// force correct input
+		while (true) {
+			int casesCheck = scan.nextInt();
+			if (casesCheck < 100 && casesCheck > 0) {
+				cases = casesCheck;
+				break;
+			}else {
+				continue;
 			}
-			System.out.println(IntStream.of(allNums).sum());
 		}
 		
+		// determine # of numbers to add
+		for (int i = 0; i < cases; i++) {
+
+			// System.out.println("How many nums are we adding?");
+			int numOfNums = scan.nextInt();
+
+		// initialize array to store numbers
+			int[] allNums = new int[numOfNums];
+
+		// determine numbers to add
+			for (int j = 0; j < numOfNums; j++) {
+				int num = scan.nextInt();
+		// store numbers into array
+				allNums[j] = num;
+			}
+		// print the result
+			System.out.println(IntStream.of(allNums).sum());
+		}
+		scan.close();
+
 	}
 
 }
