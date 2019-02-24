62616749999c226eed4b1c491ebd22eb6c822ff3
Adrian Falenczyk <falenczyk.adrian@gmail.com>
2019-02-24 16:52:52 +0100

    Create XOR.java

---
+++ XOR.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+
+class XOR
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scan = new Scanner(System.in);
+		
+		int one = scan.nextInt();
+		int two = scan.nextInt();
+		
+		System.out.println(one ^ two);
+	}
+}
