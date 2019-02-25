e8ffda331ce85ae44c4281db4cd032f3b6e07b1d
Adrian Falenczyk <falenczyk.adrian@gmail.com>
2019-02-25 12:08:42 +0100

    Act7

---
+++ NestedLoops7.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+class NestedLoops7 {
+	public static void main(String[] args) throws java.lang.Exception {
+
+		Scanner scan = new Scanner(System.in);
+
+		int cases = scan.nextInt();
+
+		for (int j = 0; j < cases; j++) {
+
+			int vertical = scan.nextInt();
+			int horizontal = scan.nextInt();
+			int size = scan.nextInt();
+
+			int tmp = 2 * size;
+			int rows = vertical * tmp;
+			int columns = horizontal * tmp;
+
+			int mod1 = size - 1;
+			int mod2 = size;
+			
+
+			for (int k = 0; k < rows; k++) {
+				
+				String oneRow = "";
+
+				for (int i = 0; i < columns; i++) {
+					
+					if (mod1 == (k + i) % tmp) {
+					
+						oneRow += '/';
+					
+					} else if (mod2 == Math.abs(k - i) % tmp) {
+					
+						oneRow += '\\';
+					
+					} else {
+					
+						oneRow += '.';
+					}
+				}
+				System.out.println(oneRow);
+			}
+			System.out.println("");
+		}
+	}
+}
