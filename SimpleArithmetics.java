01e2d3b5d4343a594b4735ac2972b9f8bfa062d9
Adrian Falenczyk <falenczyk.adrian@gmail.com>
2019-02-27 11:20:53 +0100

    Update
    added * operation formatting, still needs whitespaces to pass tests

--- SimpleArithmetics.java
+++ SimpleArithmetics.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class SimpleArithmetics {
+class SimpleArithmetics {
 
 	public static void main(String[] args) {
 
@@ -17,16 +17,28 @@ public class SimpleArithmetics {
 
 			String input = scan.nextLine();
 
-			int first = Integer.parseInt(input.substring(0, indexOf(regex, input)));
+			long first = Integer.parseInt(input.substring(0, indexOf(regex, input)));
 			String operator = Character.toString(input.charAt(indexOf(regex, input)));
-			int second = Integer.parseInt(input.substring(indexOf(regex, input) + 1, input.length()));
-			int result = 0;
-			
+			long second = Integer.parseInt(input.substring(indexOf(regex, input) + 1, input.length()));
+			long result = 0;
+
+			int firstLineLength = String.valueOf(first).length();
+			int secondLineLength = String.valueOf(second).length();
+
+			int repeats = secondLineLength;
+			int[] expressions = new int[repeats];
+
 			switch (operator.charAt(0)) {
 			case '+':
 				result += first + second;
 				break;
 			case '*':
+
+				for (int i = repeats - 1; i >= 0; i--) {
+
+					int reversedIndex = 0;
+					expressions[i] = (int) (first * Character.getNumericValue(Integer.toString((int) second).charAt(i)));
+				}
 				result += first * second;
 				break;
 			case '-':
@@ -37,31 +49,61 @@ public class SimpleArithmetics {
 				break;
 			}
 			String firstLine = "";
-			String secondLine = operator+second;
-			
-			int firstLineLength = String.valueOf(first).length();
-			int secondLineLength = String.valueOf(second).length();
-			
-			for(int filler = 0; filler <= secondLineLength-firstLineLength; filler++) {
-				firstLine += " ";
+			String secondLine = "";
+
+			if (operator.charAt(0) != '*') {
+				if (firstLineLength > secondLineLength) {
+					for (int filler = 0; filler < firstLineLength - secondLineLength - 1; filler++) {
+						secondLine += " ";
+					}
+
+				} else {
+					for (int filler = 0; filler <= secondLineLength - firstLineLength; filler++) {
+						firstLine += " ";
+					}
+				}
+				secondLine += operator + second;
+				firstLine += first;
+
+				System.out.println(firstLine);
+				System.out.println(secondLine);
+				createLine(0, secondLine.length());
+				System.out.println(result);
+			} else {
+				if (firstLineLength > secondLineLength) {
+					for (int filler = 0; filler < firstLineLength - secondLineLength + expressions.length; filler++) {
+						secondLine += " ";
+						firstLine += " ";
+					}
+				} else {
+					for (int filler = 0; filler <= secondLineLength - firstLineLength + expressions.length
+							- 1; filler++) {
+						firstLine += " ";
+						secondLine += " ";
+					}
+				}
+				secondLine += operator + second;
+				firstLine += first;
+				System.out.println(firstLine);
+				System.out.println(secondLine);
+				createLine(expressions.length + 1, secondLine.length());
+				for (int i = expressions.length - 1; i >= 0; i--) {
+//					createLine()
+					System.out.println(expressions[i]);
+				}
+				createLine(0, String.valueOf(result).length());
+				System.out.println(result);
+
 			}
-			
-			
-			firstLine += first;
-		
-			System.out.println(firstLine);
-			System.out.println(secondLine);
-			createLine(secondLineLength);
-			System.out.println(result);
-			
+			System.out.println("");
 			cases--;
 		}
 	}
 
-	private static void createLine(int length) {
+	private static void createLine(int start, int length) {
 		String line = "";
-		for(int i = 0; i < length; i++) {
-			line+= "-";
+		for (int i = 0; i < length; i++) {
+			line += (i < start) ? " " : "-";
 		}
 		System.out.println(line);
 	}
@@ -70,5 +112,4 @@ public class SimpleArithmetics {
 		Matcher matcher = pattern.matcher(string);
 		return matcher.find() ? matcher.start() : -1;
 	}
-
 }
